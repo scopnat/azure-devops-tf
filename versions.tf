a0d9f35fca85f94bd731d1d763eb88a5230d34b3
Adedeji.Awolesi <scopnatinc@hotmail.com>
2024-08-07 11:13:54 +0100

    change storage

--- versions.tf
+++ versions.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "rg-terraform"
-    storage_account_name = "stconstososalesterraform"
+    storage_account_name = "scopnatterraform"
     container_name       = "infrastructure"
     key                  = "infra-dev.tfstate"
   }
